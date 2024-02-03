cd4cb46fc04e6c33ab3110cd18bcbe7d8e140675
keidson designer <keidsondesigner@gmail.com>
2024-02-02 23:01:34 -0400

    feat: :lipstick: Estilização StatusBar

--- App.tsx
+++ App.tsx
@@ -1,18 +1,26 @@
-import { ThemeProvider } from 'styled-components';
-import theme from '@theme/index';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components";
+import theme from "@theme/index";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Loading } from '@components/Loading'
-import { Groups } from '@screens/Groups';
+import { Loading } from "@components/Loading";
+import { Groups } from "@screens/Groups";
 
 export default function App() {
-
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
-  
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups /> : <Loading /> }
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
