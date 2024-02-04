412422c8fb8910d225d6c8a24211479bf65ac4dd
keidson designer <keidsondesigner@gmail.com>
2024-02-03 20:53:54 -0400

    feat: :sparkles: Iterando sobre ListaGroups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { TitleWithSubtitle } from '@components/TitleWithSubtitle';
 import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Time 1', 'Time 2', 'Time 3']);
 
   return (
     <Container>
@@ -12,7 +16,13 @@ export function Groups() {
         title='Turmas'
         subtitle='jogue com sua turma'
       />
-      <GroupCard title='Time 1'/>
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+      />
     </Container>
   );
 }
