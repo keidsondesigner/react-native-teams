f139c5586da16f2797deeceb0a42cdd29911714b
Keidson Roby <keidsondesigner@gmail.com>
2024-02-22 22:52:34 -0400

    feat: :sparkles: Implementação Rota Voltar para o Inicio do App

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from '@react-navigation/native';
+
 import { Container, Logo, BackButton, BackIcon } from './styles';
 
 import logoImg from '@assets/logo.png'
@@ -7,11 +9,17 @@ type Props = {
 }
 
 export function Header({ showBackButton = false }: Props ) {
+  const navigation = useNavigation();
+
+  function handleGoHome() {
+    navigation.navigate('groups');
+  }
+
   return (
     <Container>
       {
         showBackButton && // se for true, mostra backButton;
-        <BackButton>
+        <BackButton onPress={handleGoHome}>
           <BackIcon />
         </BackButton>
       }
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,8 +4,15 @@ import { Header } from '@components/Header';
 import { TitleWithSubtitle } from '@components/TitleWithSubtitle';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { useNavigation } from '@react-navigation/native';
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleGoToPalyers() {
+    navigation.navigate('players', { group: 'Grupo Nome' });
+  }
+
   return(
     <Container>
       <Header showBackButton />
@@ -21,6 +28,7 @@ export function NewGroup() {
         <Button
           title='Criar'
           style={{ marginTop: 20}}
+          onPress={handleGoToPalyers}
         />
       </Content>
     </Container>
